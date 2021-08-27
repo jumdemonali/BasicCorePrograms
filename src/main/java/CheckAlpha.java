22f932d58cf75d1da32a9f59001674a7193756e1
jumdemonali <jumdemonali@gmail.com>
2021-08-27 15:57:00 +0530

    Added:Checking alphabate is vowel or consonant

---
+++ src/main/java/CheckAlpha.java
@@ -0,0 +1,10 @@
+public class CheckAlpha {
+    public static void main(String[] args) {
+        char ch = 'u';
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is an vowel");
+        else
+            System.out.println(ch + " is an consonant");
+    }
+
+}
